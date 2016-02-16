7763d9d2b7576cf4088e622548f63d9cd6d79fa6
joseph-darcy <joseph.darcy@maine.edu>
2016-02-15 20:27:16 -0500

    Adds a hello message

--- helloworld 2/src/HelloWorld.java
+++ helloworld 2/src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Here ye, here ye important announcement: ");
 		System.out.println("Hello World!");
 		
 
