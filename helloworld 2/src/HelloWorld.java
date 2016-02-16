05a5b65ab790d186751b4aa59a6eda2740b8bd95
joseph-darcy <joseph.darcy@maine.edu>
2016-02-15 20:28:06 -0500

    Adds a bye message

--- helloworld 2/src/HelloWorld.java
+++ helloworld 2/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Here ye, here ye important announcement: ");
 		System.out.println("Hello World!");
+		System.out.println("Thank you for your time.");
 		
 
 	}
