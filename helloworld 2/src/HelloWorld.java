1a3cbdd36947b717dd5a5220940393783fb03e49
joseph-darcy <joseph.darcy@maine.edu>
2016-02-15 20:48:09 -0500

    adds more to the preamble

--- helloworld 2/src/HelloWorld.java
+++ helloworld 2/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Here ye, here ye important announcement: ");
+		System.out.println("this is very important");
 		System.out.println("Hello World!");
 		System.out.println("Thank you for your time.");
 		
